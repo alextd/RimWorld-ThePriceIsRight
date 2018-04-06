feded83acad400e503933f06952cb5389a245267
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-06 14:40:43 -0700

    Extra mood buff if caravan has beds

--- Source/TravelingMood.cs
+++ Source/TravelingMood.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Verse;
 using RimWorld;
+using RimWorld.Planet;
 
 namespace The_Price_Is_Right
 {
@@ -16,13 +17,27 @@ namespace The_Price_Is_Right
 
 	public class ThoughtWorker_Traveling : ThoughtWorker
 	{
-		protected override ThoughtState CurrentStateInternal(Pawn p)
+		protected override ThoughtState CurrentStateInternal(Pawn pawn)
 		{
-			if (p.Map != null && p.Map.IsPlayerHome)
+			if (pawn.Map != null && pawn.Map.IsPlayerHome)
 			{
 				return ThoughtState.Inactive;
 			}
+			if(pawn.GetCaravan() is Caravan caravan)
+			{
+				List<Pawn> carvanPawns = caravan.PawnsListForReading;
+				int bedCount = carvanPawns.Select(p => CountBeds(p)).Sum();
+				int colonistCount = carvanPawns.FindAll(p => p.IsColonist).Count;
+				if (bedCount >= colonistCount)
+					return ThoughtState.ActiveAtStage(1);
+
+			}
 			return ThoughtState.ActiveAtStage(0);
+		}
+
+		public static int CountBeds(Pawn pawn)
+		{
+			return pawn.inventory.innerContainer.Where(t => t.GetInnerIfMinified() is Building_Bed).Count();
 		}
 	}
 }
