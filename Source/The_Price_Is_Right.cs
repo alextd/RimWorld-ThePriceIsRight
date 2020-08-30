b49692307bcb619a74daf3de564abee03b61e88c
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-08-30 13:21:53 -0700

    removing DefOf Warning suppression. Seems to not be a problem anymore.

--- Source/The_Price_Is_Right.cs
+++ Source/The_Price_Is_Right.cs
@@ -18,18 +18,7 @@ namespace The_Price_Is_Right
 #endif
 			Harmony harmony = new Harmony("Uuugggg.rimworld.The_Price_Is_Right.main");
 			
-			//Turn off DefOf warning since harmony patches trigger it.
-			MethodInfo DefOfHelperInfo = AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor");
-			if (!harmony.GetPatchedMethods().Contains(DefOfHelperInfo))
-				harmony.Patch(DefOfHelperInfo, new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
-			
 			harmony.PatchAll();
-		}
-
-		public static bool EnsureInitializedInCtorPrefix()
-		{
-			//No need to display this warning.
-			return false; 
 		}
 
 		public override void DoSettingsWindowContents(Rect inRect)
